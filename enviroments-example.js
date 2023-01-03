3d2ec7ee292f1aa9b415a664d970baf517ce7878
luisangelorjr <luisangelorjr@gmail.com>
2023-01-03 10:06:49 -0300

    ADD: Fully functional v0.1.0

---
+++ enviroments-example.js
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    urlWebPontoLoginEmpresa: "https://admin.oitchau.com.br/sso-login",
+    urlWebPontoPunchEmpresa: "https://admin.oitchau.com.br/punches/",
+    login: "email@exemplo.com",
+    password: "123admin"
+  };
--- oitchau.mjs
+++ oitchau.mjs
@@ -1,12 +1,12 @@
 import puppeteer from 'puppeteer';
+import { environment } from './enviroments';
 
 const headlessMode = false;
 
-const urlWebPontoLoginEmpresa = "https://admin.oitchau.com.br/sso-login";
-const urlWebPontoPunchEmpressa = "https://admin.oitchau.com.br/punches/";
-const codEmpresa = "999";
-const login = "re042784@qintess.com";
-const password = "k4js6z2Y!";
+const urlWebPontoLoginEmpresa = environment.urlWebPontoLoginEmpresa;
+const urlWebPontoPunchEmpresa = environment.urlWebPontoPunchEmpresa;
+const login = environment.login;
+const password = environment.password;
 
 (async () => {
     const browser = await puppeteer.launch(({ headless: headlessMode }));
@@ -17,7 +17,8 @@ const password = "k4js6z2Y!";
     await page.type('input[name=email]', login);
     await page.click('button');
 
-    //Login SSO Microsoft
+    // Login SSO Microsoft
+    // START
     await page.waitForSelector('input[name=loginfmt]');
     await page.type('input[name=loginfmt]', login);
     await page.click('input[type=submit]');
@@ -26,31 +27,38 @@ const password = "k4js6z2Y!";
     await page.type('input[name=passwd]', password);
 
     console.log('before waiting');
-    await delay(4000);
+    await delay(2000);
     console.log('after waiting');
+    
 
     await page.waitForSelector('input[id=idSIButton9]');
     await page.click('input[id=idSIButton9]');
 
     console.log('before waiting');
-    await delay(4000);
+    await delay(2000);
     console.log('after waiting');
 
     await page.waitForSelector('input[id=idSIButton9]');
     await page.click('input[id=idSIButton9]');
+    // Login SSO Microsoft
+    // END
+
+    console.log('before waiting');
+    await delay(8000);
+    console.log('after waiting');
 
     await page.waitForSelector('button[data-for=web-punch-button]');
     await page.click('button[data-for=web-punch-button]');
 
-    //<button data-tip="true" data-for="web-punch-button" class="PunchNowButton-sc-1weavqm-1 fuWjdI" currentitem="false" aria-describedby="teded704e-86ca-435c-9583-2b4398141b47"><img id="web-punch-button" src="/static/media/logo.4571cb05.png" alt="Bater ponto"></button>
-    //<button class="Button-sc-jryu0u-2 iTejjP"></button>
 
-    await page.waitForSelector('button[class=Button-sc-jryu0u-2]');
-    await page.click('button[class=Button-sc-jryu0u-2]');
+    console.log('before waiting');
+    await delay(2000);
+    console.log('after waiting');
 
-    await page.screenshot({ path: `screenshots/pptr-empresa-ponto/${new Date().toISOString()}.jpg`, type: 'jpeg', quality: 90 });
+    await page.waitForSelector('button[class=iTejjP]');
+    await page.click('button[class=iTejjP]');
 
-    await page.click('.baterPontoBtn');
+    await page.screenshot({ path: `screenshots/${new Date().toISOString()}.jpg`, type: 'jpeg', quality: 90 });
 
     console.log("THE END");
     await browser.close();
