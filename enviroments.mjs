cdf830f7693de13f9c7ea7f91cdb1348ae9a0886
luisangelorjr <luisangelorjr@gmail.com>
2023-01-06 19:25:49 -0300

    FIX: '''improvements'''

--- enviroments.mjs
+++ enviroments.mjs
@@ -1,5 +1,9 @@
 export const environment = {
     production: true,
+    tratamento: "Sr.",
+    firstName: "Luis Ângelo",
+    lastName: "Rodrigues Jr.",
+    nickname: "luisangelorjr",
     urlWebPontoLoginEmpresa: "https://admin.oitchau.com.br/sso-login",
     urlWebPontoPunchEmpresa: "https://admin.oitchau.com.br/punches/",
     login: "re042784@qintess.com",
--- oitchau.mjs
+++ oitchau.mjs
@@ -1,19 +1,27 @@
 import puppeteer from 'puppeteer';
 import { environment } from './enviroments.mjs';
+import * as fs from 'fs';
 
-const headlessMode = false;
+const headlessMode = environment.production;
 
 const urlWebPontoLoginEmpresa = environment.urlWebPontoLoginEmpresa;
 const urlWebPontoPunchEmpresa = environment.urlWebPontoPunchEmpresa;
+const tratamento = environment.tratamento;
+const firstName = environment.firstName;
+
 const login = environment.login;
 const password = environment.password;
 
 (async () => {
+    console.log(`${localISOTime()} - Bom dia ${tratamento} ${firstName}!`);
+    console.log(`${localISOTime()} - Sou Odrama J. Anairam! O seu CHRO pessoal para bater ponto`)
+    console.log(`${localISOTime()} - Sem mais delongas, vou começar agora`)
     const browser = await puppeteer.launch(({ headless: headlessMode }));
     const page = await browser.newPage();
 
     await page.goto(urlWebPontoLoginEmpresa);
 
+    await page.waitForSelector('input[name=email]');
     await page.type('input[name=email]', login);
     await page.click('button');
 
@@ -26,51 +34,53 @@ const password = environment.password;
     await page.waitForSelector('input[name=passwd]');
     await page.type('input[name=passwd]', password);
 
-    console.log('before waiting');
-    await delay(2000);
-    console.log('after waiting');
-    
+
+    await delay(2000, "Aguardando botão 01 de confirmação Microsoft");
 
     await page.waitForSelector('input[id=idSIButton9]');
     await page.click('input[id=idSIButton9]');
 
-    console.log('before waiting');
-    await delay(2000);
-    console.log('after waiting');
+    await delay(2000, "Aguardando botão 02 de confirmação Microsoft");
 
     await page.waitForSelector('input[id=idSIButton9]');
     await page.click('input[id=idSIButton9]');
     // Login SSO Microsoft
     // END
 
-    console.log('before waiting');
-    await delay(8000);
-    console.log('after waiting');
+    await delay(8000, "Aguardando botão flutuante da oitchau");
 
     await page.waitForSelector('button[data-for=web-punch-button]');
     await page.click('button[data-for=web-punch-button]');
 
+    await delay(2000, "Aguardando botão de 'Marcar Ponto' da oitchau");
 
-    console.log('before waiting');
-    await delay(2000);
-    console.log('after waiting');
+    const today = localISOTime().toString().substring(0, 10);
+    if (!fs.existsSync(`screenshots/${today}`)){
+        fs.mkdirSync(`screenshots/${today}`);
+    }
 
     await page.waitForSelector('.iTejjP');
     await page.click('.iTejjP');
 
-    console.log('before waiting');
-    await delay(2000);
-    console.log('after waiting');
+    await delay(2000, "Aguardando tela com ponto marcado da oitchau para print");
 
-    await page.screenshot({ path: `screenshots/${new Date().toISOString()}.jpg`, type: 'jpeg', quality: 90 });
+ 
+    await page.screenshot({ path: `screenshots/${today}/${localISOTime()}.jpg`, type: 'jpeg', quality: 90 });
+    console.log(`${localISOTime()} - ${tratamento} ${firstName}, bati seu ponto com sucesso agorinha`);
+    console.log(`${localISOTime()} - To indo agora. Desculpa qualquer coisa`);
 
-    console.log("THE END");
     await browser.close();
 })();
 
-function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
-    });
+function delay(time, texto) {
+    console.log(`${localISOTime()} - ${texto.toUpperCase()} - Inicio da chamada`);
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time);
+        console.log(`${localISOTime()} - ${texto.toUpperCase()} - Término da chamada`);
+    })
  }
  
+ function localISOTime(){
+    const tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    return new Date(Date.now() - tzoffset).toISOString().slice(0, -1);
+}
